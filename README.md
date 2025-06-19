e4dc7aeaa1628111e951f6a103f96b1d76f019ad
Dejan Spasic <dejan.spasic@bitweise.biz>
2025-06-20 00:44:14 +0200

    completed create a module

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Personal Go Playground
 ## Todo's
 
 * Get the basic
+* * Continue here: https://go.dev/doc/tutorial/workspaces
 * Read [Effective Go](https://go.dev/doc/effective_go)
--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -10,13 +10,13 @@ func TestHelloName(t *testing.T) {
 	want := regexp.MustCompile(`\b` + name + `\b`)
 	msg, err := Hello(name)
 	if !want.MatchString(msg) || err != nil {
-		t.Fatalf(`Hello(%q) = %q, %v want match for %#q, nil`, name, msg, err, want)
+		t.Errorf(`Hello(%q) = %q, %v want match for %#q, nil`, name, msg, err, want)
 	}
 }
 
 func TestHelloEmpty(t *testing.T) {
 	msg, err := Hello("")
 	if msg != "" || err == nil {
-		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+		t.Errorf(`Hello("") = %q, %v, want "", error`, msg, err)
 	}
 }
